63c267a156b1f06bd0881784bce89c536a784772
ParnanenJ <jiro.parnanen@student.laurea.fi>
2025-11-11 19:46:15 +0200

    comments added

--- script.js
+++ script.js
@@ -42,15 +42,18 @@ infobtn.addEventListener("click", function(){
 ///////////////////////////////////////////////////////////
 
 const logo = document.getElementById("logo");
-
 const lomake = document.getElementById("lomake");
+
+// Lomakkeen lähettäminen
 lomake.addEventListener('submit', function(e) {
     e.preventDefault(); // estää sivun latautumisen
 
+    // otetaan hausta turhat välilyönnit pois ja muutetaan teksti isoiksi kirjaimiksi
     let syote = lomake.elements["kentta"].value.trim().toUpperCase();
 
+    // tarkistetaan ettei hakukenttä ole tyhjä
     if (syote.length === 0) {
-        alert("Kenttä ei voi olla tyhjä!");
+        document.getElementById("error").innerHTML = "No stocks found"
         return;
     }
 
@@ -58,18 +61,25 @@ lomake.addEventListener('submit', function(e) {
     ///////////////////////////////////////////////////////////
     // 1 API KUTSU
 
+    // luodaan uusi XMLHttpRequest-objekti
     var tieto = new XMLHttpRequest();
+    // tehdään GET api-kutsu
     tieto.open('GET', `https://financialmodelingprep.com/stable/profile?symbol=${syote}&apikey=SNVbYoxdJgjGTpUbtP0U1Hj8eMs1eEtv`, true);
 
+    // kun tieto saapuu niin suoritetaan funktio
     tieto.onreadystatechange = function() {
+        // jos ei tule virheitä -> jatketaan suorittamista
         if (tieto.readyState === 4 && tieto.status === 200) {
+            // tallennetaan saapunut data muuttujaan
             let data = JSON.parse(tieto.responseText);
 
+            // jos data on tyhjä -> pysäytetään suoritus ja annetaan käyttäjälle virhe teksti
             if (data.length === 0){
                 lukko.style.display = "none";
                 document.getElementById("error").innerHTML = "No stocks found"
                 return;
             }
+            // kun data ei ole tyhjä -> jatketaan suorittamista ja poistetaan mahdollinen virhe teksti
             document.getElementById("error").innerHTML = ""
 
             // Näytetään loppuosa sivusta
@@ -97,10 +107,10 @@ lomake.addEventListener('submit', function(e) {
             // Pörssilistaus
              document.getElementById("porssi").textContent = data[0].exchange;
 
-            // Tallennetaan valuutta
+            // Tallennetaan valuutta globaaliin muuttujaan
             valuutta = data[0].currency;
 
-            // tallennetaan nykyinen hinta
+            // tallennetaan nykyinen hinta globaaliin muuttujaan ja tulostetaan se
             nykhinta = parseFloat(data[0].price);
             document.getElementById("hinta").textContent = nykhinta + " " + valuutta;
 
@@ -122,37 +132,42 @@ lomake.addEventListener('submit', function(e) {
     endhinnat.length = 0;
     paivamaarat.length = 0;
 
-    // Nollataan rajoitetut tiedot ettei tosen osakkeen tiedot jää näkyviin jos käyttäjä hakee toista osaketta joole ei ole saatavissa hinta tietoja
+    // Nollataan rajoitetut tiedot ettei toisen osakkeen tiedot jää näkyviin jos käyttäjä hakee toista osaketta jolle ei ole saatavissa hinta tietoja
     document.getElementById("aloitushinta").textContent = "-";
     document.getElementById("ylin").textContent = "-";
     document.getElementById("alin").textContent = "-";
 
     // Jos ticker löytyy limited listasta haetaan tarkemmat hintatiedot
     if (limited.includes(syote)){
+        // luodaan uusi XMLHttpRequest-objekti
         var hintaTieto = new XMLHttpRequest();
+        // tehdään GET api-kutsu
         hintaTieto.open('GET', `https://financialmodelingprep.com/stable/historical-price-eod/full?symbol=${syote}&apikey=SNVbYoxdJgjGTpUbtP0U1Hj8eMs1eEtv`, true);
 
+        // kun tieto saapuu niin suoritetaan funktio
         hintaTieto.onreadystatechange = function() {
+            // jos ei tule virheitä -> jatketaan suorittamista
             if (hintaTieto.readyState === 4 && hintaTieto.status === 200) {
+                // tallennetaan saapunut data muuttujaan
                 let hintadata = JSON.parse(hintaTieto.responseText);
 
 
                 ///////////////////////////////////////////////////////////
                 //TIEDON KERUU
-                // tallennetaan Päivän avushinta
+                // tallennetaan Päivän avushinta globaaliin muuttujaan ja tulostetaan se
                 avshinta = parseFloat(hintadata[0].open);
                 document.getElementById("aloitushinta").textContent = `(Open ${avshinta} ${valuutta})`;
-                // Päivän Ylin hinta
+                // Tallennetaan päivän Ylin hinta globaaliin muuttujaan ja tulostetaan se
                 maxHinta = parseFloat(hintadata[0].high)
                 document.getElementById("ylin").textContent = maxHinta + " " + valuutta;
-                // Päivän Alin hinta
+                // Tallennetaan päivän Alin hinta globaaliin muuttujaan ja tulostetaan se
                 minHinta = parseFloat(hintadata[0].low)
                 document.getElementById("alin").textContent = minHinta + " " + valuutta;
 
             
                 ///////////////////////////////////////////////////////////
                 // laskut
-                // Lasketaan tuottoprosentti ja muokataan tyyli -/+
+                // Lasketaan tuottoprosentti ja muokataan tyyli -/+ prosenttilasku funktion avulla
                 prosenttilaskut(avshinta, nykhinta, "pricemuutos")
                 prosenttilaskut(avshinta, maxHinta, "maxmuutos")
                 prosenttilaskut(avshinta, minHinta, "minmuutos")
@@ -195,21 +210,25 @@ lomake.addEventListener('submit', function(e) {
                 ///////////////////////////////////////////////////////////
             };
         };
-        hintaTieto.send();
+        hintaTieto.send(); // Kutsu
     };
 });
 
 ///////////////////////////////////////////////////////////
-    // Alasvetovalikon toiminnallisuus
+    // chatin alasvetovalikon toiminnallisuus
 
 const alasveto = document.getElementById("alasveto");
 
+// kun chartin aikaväliä muutetaan
 alasveto.addEventListener("change", function(){
+    // muutetaan arvo numeroksi ja tallennetaan se muuttujaan
     const arvo = Number(alasveto.value);
 
+    // otetaan tietty määrä päiviä mukaan charttiin arvon mukaan
     const pv = paivamaarat.slice(0, arvo);
     const hinnat = endhinnat.slice(0, arvo);
 
+    // luodaan chart funktion avulla
     taulukonLuonti(pv, hinnat);
 });
 
@@ -235,6 +254,7 @@ alasveto.addEventListener("change", function(){
             element.classList.remove('bg-success');
             element.classList.add('bg-danger');
             icon.className = 'fas fa-arrow-down me-1';
+        // jos positiivinen
         } else {
             // muokataan tyyli
             element.classList.remove('bg-danger');
@@ -248,7 +268,6 @@ alasveto.addEventListener("change", function(){
 
 
     ///////////////////////////////////////////////////////////
-    // Taulukon luonti funktio
     // Taulukon luonti funktio
     function taulukonLuonti(pv, hinta) {
         // Hinta chart
@@ -270,13 +289,12 @@ alasveto.addEventListener("change", function(){
             chart.chart = new Chart(ctx, {
                 type: "line",
                 data: {
-                    labels: pv,
+                    labels: pv, // lisätään päivämäärät datapisteille
                     datasets: [{
-                        label: "End ($)",
-                        fill: false,           // HUOM: jos fill = true, viiva saattaa piiloutua gradientin alle
+                        fill: false,   
                         backgroundColor: gradient,
-                        borderColor: "#007bff",
-                        data: hinta,
+                        borderColor: "#6366f1", // käyrän väri
+                        data: hinta, // lisätään data datapisteille
                         pointRadius: 0,
                         pointHoverRadius: 0,
                         hitRadius: 10
@@ -285,8 +303,8 @@ alasveto.addEventListener("change", function(){
                 options: {
                     maintainAspectRatio: false,
                     interaction: {
-                        mode: 'index',    // Hover seuraa x-akselin mukaista datapistettä
-                        axis: 'x',        // Pystysuora viiva
+                        mode: 'index',  
+                        axis: 'x',        
                         intersect: false
                     },
                     plugins: {
